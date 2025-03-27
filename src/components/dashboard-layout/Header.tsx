7795280a9ce645d2efbc1ed8b260523a33d91fcb
Kate <e.khizova@gmail.com>
2025-03-27 18:07:48 +0300

    feat: add header

--- src/components/dashboard-layout/Header.tsx
+++ src/components/dashboard-layout/Header.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './header.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../../public/logo.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='container'>
+      
+      <div className={styles.buttons_container}>
+      <Link href='/'>
+        <Image priority={true} width={30} height={30} src={logo} alt='logo' />
+      </Link>
+        <p className={styles.logo}>Rest Client</p>
+      </div>
+      <div className={styles.buttons_container}>
+        <Link href='/' className={styles.button}>Home</Link>
+        <Link href='/about' className={styles.button}>About</Link>
+      </div>
+      <div className={styles.buttons_container}>
+        <select className={styles.button}>
+          <option value="En">En</option>
+          <option value="Ru">Ru</option>
+        </select>
+        <Link href='/auth' className={styles.button}>Sign in</Link>
+        <Link href='/auth' className={styles.button}>Login</Link>
+      </div>
+  </header>;
 };
 
 export default Header;
