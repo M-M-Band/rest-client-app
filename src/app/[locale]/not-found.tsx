6793b29756a77a488ca90d87706240ff651f6a06
Kate <e.khizova@gmail.com>
2025-04-05 13:04:59 +0300

    feat: add not found page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,28 @@
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import img from '@/../public/404.png';
+
+export default function NotFound() {
+  const t = useTranslations('404');
+  return (
+    <div>
+      <h1 className='maintext_green'>{t('title')}</h1>
+      <p className='subtext'>{t('description')}</p>
+      <Link
+        href='/'
+        className='button button_colored'
+      >
+        {t('backButton')}
+      </Link>
+      <Image
+        priority={true}
+        width={500}
+        height={400}
+        src={img}
+        alt='logo'
+      />
+    </div>
+  );
+}
