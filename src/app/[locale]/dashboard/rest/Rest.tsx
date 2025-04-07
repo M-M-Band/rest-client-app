3fdf15fa15577d70c1119bf6a3fc2a6bb9c880ff
AlexKabanau <azraill1989@gmail.com>
2025-04-07 12:35:54 +0300

    fix: add show/hide headers and hide body fild

--- src/app/[locale]/dashboard/rest/Rest.tsx
+++ src/app/[locale]/dashboard/rest/Rest.tsx
@@ -2,7 +2,12 @@
 
 import { useActionState, useEffect, useRef, useState } from 'react';
 
-import { HTTP_METHODS, Header, initialState } from '@/types/rest.types';
+import {
+  HTTP_METHODS,
+  Header,
+  METHODS_WITH_BODY,
+  initialState,
+} from '@/types/rest.types';
 
 import { sendRequest } from '@/utils/rest-actions';
 
@@ -36,6 +41,7 @@ const Rest = () => {
   ]);
   const [body, setBody] = useState('');
   const [method, setMethod] = useState('GET');
+  const [showHeaders, setShowHeaders] = useState(true);
 
   const inputTableRefs = useRef<(HTMLInputElement | null)[]>([]);
   const inputSearchRef = useRef<HTMLInputElement | null>(null);
@@ -117,8 +123,8 @@ const Rest = () => {
               ref={inputSearchRef}
               name='url'
               type='url'
-              defaultValue='https://jsonplaceholder.typicode.com/posts'
-              placeholder='https://jsonplaceholder.typicode.com'
+              defaultValue='https://650abf4edfd73d1fab08cfdc.mockapi.io/items'
+              placeholder='https://650abf4edfd73d1fab08cfdc.mockapi.io/items'
               required
             />
           </div>
@@ -131,7 +137,12 @@ const Rest = () => {
           </button>
         </div>
         <div className={`${container} ${container_nested}`}>
-          <h2>Headers:</h2>
+          <h2
+            onClick={() => setShowHeaders((prev) => !prev)}
+            style={{ cursor: 'pointer' }}
+          >
+            {showHeaders ? '▼' : '▶'} Headers:
+          </h2>
           <button
             className={`button ${button_border}`}
             type='button'
@@ -140,63 +151,68 @@ const Rest = () => {
             Add header
           </button>
         </div>
-        <table className={headers__table}>
-          <thead>
-            <tr>
-              <th>Key</th>
-              <th>Value</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {headers.map((header, index) => (
-              <tr key={index}>
-                <td>
-                  <input
-                    type='text'
-                    placeholder='Header name'
-                    value={header.key}
-                    onChange={(e) =>
-                      handleHeaderChange(index, 'key', e.target.value)
-                    }
-                    ref={setInputRef(index)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type='text'
-                    placeholder='Header value'
-                    value={header.value}
-                    onChange={(e) =>
-                      handleHeaderChange(index, 'value', e.target.value)
-                    }
-                  />
-                </td>
-                <td>
-                  <button
-                    type='button'
-                    className={`button ${button_border}`}
-                    onClick={() => removeHeader(index)}
-                  >
-                    X
-                  </button>
-                </td>
+        {showHeaders && (
+          <table className={headers__table}>
+            <thead>
+              <tr>
+                <th>Key</th>
+                <th>Value</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {headers.map((header, index) => (
+                <tr key={index}>
+                  <td>
+                    <input
+                      type='text'
+                      placeholder='Header name'
+                      value={header.key}
+                      onChange={(e) =>
+                        handleHeaderChange(index, 'key', e.target.value)
+                      }
+                      ref={setInputRef(index)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type='text'
+                      placeholder='Header value'
+                      value={header.value}
+                      onChange={(e) =>
+                        handleHeaderChange(index, 'value', e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <button
+                      type='button'
+                      className={`button ${button_border}`}
+                      onClick={() => removeHeader(index)}
+                    >
+                      X
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+
         <div className={`${container} ${container_nested}`}>
           <h2>Code:</h2>
         </div>
-        <div className={`${container} ${container_requestbody}`}>
-          <h2>Body:</h2>
-          <textarea
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            placeholder='Request body (JSON)'
-            rows={6}
-          ></textarea>
-        </div>
+        {METHODS_WITH_BODY.includes(method) && (
+          <div className={`${container} ${container_requestbody}`}>
+            <h2>Body:</h2>
+            <textarea
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              placeholder='Request body (JSON)'
+              rows={6}
+            ></textarea>
+          </div>
+        )}
       </form>
       <div className={response}>
         <h2>Response: </h2>
